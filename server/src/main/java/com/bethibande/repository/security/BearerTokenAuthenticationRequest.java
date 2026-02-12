429d218b836736a84dafb8b82a5f0445ddc39d99
bethibande <bethibande@gmail.com>
2026-02-12 15:02:24 +0100

    Kubernetes Gateway API support for routing #36

--- server/src/main/java/com/bethibande/repository/security/BearerTokenAuthenticationRequest.java
+++ server/src/main/java/com/bethibande/repository/security/BearerTokenAuthenticationRequest.java
@@ -1,7 +1,9 @@
 package com.bethibande.repository.security;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import io.quarkus.security.identity.request.BaseAuthenticationRequest;
 
+@RegisterForReflection
 public class BearerTokenAuthenticationRequest extends BaseAuthenticationRequest {
 
     private final String accessToken;

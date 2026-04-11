4ef2f8e1952f0f2936cd1698cea7f0d3b57ac586
bethibande <bethibande@gmail.com>
2026-04-11 14:13:21 +0200

    fix native-image reflection issue

--- server/src/main/java/com/bethibande/arcae/repository/helm/HelmIndex.java
+++ server/src/main/java/com/bethibande/arcae/repository/helm/HelmIndex.java
@@ -1,9 +1,12 @@
 package com.bethibande.arcae.repository.helm;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 
+@RegisterForReflection
 public record HelmIndex(
         String apiVersion,
         Map<String, List<HelmIndexEntry>> entries,
